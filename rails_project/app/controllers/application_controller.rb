f94330e43c812b7d17b5183bbff6f757dd378034
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2021-07-28 11:00:14 -0500

    reworked nav

--- rails_project/app/controllers/application_controller.rb
+++ rails_project/app/controllers/application_controller.rb
@@ -2,12 +2,15 @@ class ApplicationController < ActionController::Base
     before_action :current_user
 
     def home
-
     end
 
     def current_user
         if session[:current_user_id]
             @current_user = User.find(session[:current_user_id])
         end
+    end
+
+    def login(user)
+        session[:current_user_id] = @user.id
     end
 end
