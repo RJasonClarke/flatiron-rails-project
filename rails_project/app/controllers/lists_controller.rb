80890923e7fd9b307825ccac3927f4fb1fa5a762
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2021-07-28 12:20:12 -0500

    create list function, index of lists

--- rails_project/app/controllers/lists_controller.rb
+++ rails_project/app/controllers/lists_controller.rb
@@ -1,2 +1,21 @@
 class ListsController < ApplicationController
+    before_action :authentication_required
+
+    def new
+    end
+
+    def create
+        @list = List.new
+        @list.title = params['Title']
+        if @list.save
+            redirect_to '/lists'
+        else
+            render 'lists/new'
+        end
+    end
+
+    def index
+        @lists = List.all
+    end
+
 end
