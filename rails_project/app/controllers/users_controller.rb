036bba504f7dd7f44ecd25c197979f8f3674b3fa
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2021-07-28 09:26:22 -0500

    created user register and created home page

--- rails_project/app/controllers/users_controller.rb
+++ rails_project/app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+        @user = User.new
+        @user.email = params[:email]
+        @user.password_digest = params[:password_digest]
+        if @user.save
+            redirect_to '/'
+        else
+            render '/register'
+        end
+    end
+
 end
--- rails_project/config/routes.rb
+++ rails_project/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+get '/register', to: 'users#new'
+post '/users', to: 'users#create'
+
+root to: 'application#home'
+
 end
