5e345f27868a68112f6dab0cdf0e6dd2ea5057b1
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2021-07-29 11:08:53 -0500

    fixed migration issue

--- rails_project/db/schema.rb
+++ rails_project/db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_28_163532) do
+ActiveRecord::Schema.define(version: 2021_07_29_155617) do
 
   create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
