e70c9bda5766e14de76004bbb5efca94ce06db65
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2021-08-09 10:58:17 -0500

    new

--- rails_project/app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-    include ApplicationHelper
-    before_action :current_user
-
-    def home
-    end
-
-end
--- rails_project/config/routes.rb
+++ rails_project/config/routes.rb
@@ -1,23 +1,5 @@
 Rails.application.routes.draw do
 
-get '/auth/github/callback', to: 'sessions#create'
-
-
-get '/register', to: 'users#new'
-get '/login', to: 'sessions#new'
-post '/sessions', to: 'sessions#create'
-get '/logout', to: 'sessions#destroy'
-
-resources :users
-
-resources :lists do
-    resources :movies, only: [:show, :index]
-end
-
-resources :movies
-
-
-
 root to: 'application#home'
 
 end
